50fb251feecb29a207c917d52d84a94398dccff9
Kevin Power <kpow@kpow.com>
2019-12-14 16:28:37 -0500

    added global validation, hide color till theme, and alternate zipcode error messages

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $( document ).ready( () => {
           $titleElement = $('#title'),
           $designElement = $('#design')
           $colorElement = $('#color'),
+          $colorLabel = $('label[for="color"]'),
           $colorOptions = $('#color > option'),
           $otherElement = $('#other-title'),
           $otherLabel = $('label[for="other"]')
@@ -17,7 +18,8 @@ $( document ).ready( () => {
           $paymentElement = $('#payment'),
           $ccElement = $('#cc_num'),
           $cvvElement = $('#cvv'),
-          $zipcodeElement = $('#zip')
+          $zipcodeElement = $('#zip'),
+          $submitButton = $('#submit')
 
     // error messages for validation
     const errorMessages = {
@@ -35,6 +37,7 @@ $( document ).ready( () => {
     
     // variable for tracking price      
     let totalPrice = 0  
+    let ccActive = true;
 
     // initial setup      
     $nameElement.focus()
@@ -43,42 +46,77 @@ $( document ).ready( () => {
     $bitcoinElement.hide()
     $otherElement.hide()
     $otherLabel.hide()
+    $colorElement.hide()
+    $colorLabel.hide()
     $colorOptions.each(function() {$(this).prop('hidden', 'hidden');})
     $colorElement.prepend('<option value="" disabled selected hidden>Please select a T-shirt theme</option>')
     $activityElement.append('<div class="total-price"></div>')
 
     // validation functions
-    const isNameValid = () => /^(?!\s*$).+/.test($nameElement.val())
+    const isNameValid = () =>{
+        const valid = /^(?!\s*$).+/.test($nameElement.val())
+        valid  ? toggleError($nameElement, 'remove') : toggleError($nameElement, 'add')
+        return valid
+    } 
 
     const isEmailValid = () =>{
         const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        return regEx.test($emailElement.val())
+        const valid = regEx.test($emailElement.val())
+        valid ? toggleError($emailElement, 'remove') : toggleError($emailElement, 'add')
+        return valid
     }
 
     const isActivitiesValid = () =>{
-        let hasActivity = false
+        let valid = false
         $activityCheckboxes.each((i)=>{
-          if($($activityCheckboxes[i]).is(':checked')) hasActivity = true
+          if($($activityCheckboxes[i]).is(':checked')) valid = true
         })
-        return hasActivity
+        valid ? toggleError($activityElement, 'remove') : toggleError($activityElement, 'add') 
+        return valid
     }
 
     const isCreditCardValid = () =>{
         const regEx = /^\d{4}([ \-]?)((\d{6}\1?\d{5})|(\d{4}\1?\d{4}\1?\d{4}))$/gm
-        return regEx.test($ccElement.val())
+        const valid = regEx.test($ccElement.val())
+        valid ? toggleError($ccElement, 'remove') : toggleError($ccElement, 'add')
+        return valid
     }
 
     const isZipCodeValid = () =>{
-        const regEx = /^\d{5}(?:[-\s]\d{4})?$/
-        return regEx.test($zipcodeElement.val())
+        const valid = /^\d{5}(?:[-\s]\d{4})?$/.test($zipcodeElement.val())
+        // checking the state of the input and changing error message based on that
+        if($zipcodeElement.val().length >= 1 && $zipcodeElement.val() < 5){
+            errorMessages.zip = "zip code must be at least 5 characters"
+        }else if($zipcodeElement.val().length < 1){
+            errorMessages.zip = "enter a valid zip code"
+        }
+
+        valid ? toggleError($zipcodeElement, 'remove') : toggleError($zipcodeElement, 'add')
+        return valid
     }
 
-    const isCVVValid = () =>{
-        const regEx = /^\d{3}?/
-        return regEx.test($cvvElement.val())
+    const isCVVValid = () =>{  
+        const valid = /^\d{3}?/.test($cvvElement.val())
+        valid ? toggleError($cvvElement, 'remove') : toggleError($cvvElement, 'add')
+        return valid
     }
 
-    // this function presednt error messaging
+    const validateForm = (e) => {
+        e.preventDefault()
+        // check universal elements
+        isNameValid()
+        isEmailValid()
+        isActivitiesValid()
+        // check these if CC is active
+        if(ccActive){
+            isCreditCardValid()
+            isZipCodeValid()
+            isCVVValid()  
+        }
+        
+    }
+
+    // this function present error messaging
     const toggleError = ($element, what) =>{
         // grab the id to create class and access error messages
         const elementID = $element.attr('id')
@@ -91,13 +129,13 @@ $( document ).ready( () => {
             $('.'+elementID+'-error').remove()
 
         }else if(what === "add"){
-            // check to see if error is up already
-            if(!$('.'+elementID+'-error').length){
-                // if not add element style and presesnt error message
-                $element.addClass('is-error')
-                $('<div class="error-message '+$element.attr('id')+'-error">'+errorMessage+'</div>')
-                 .insertAfter($element)
-            }
+            // remove any existing error
+            $('.'+elementID+'-error').remove()
+            // diplay error
+            $element.addClass('is-error')
+            $('<div class="error-message '+$element.attr('id')+'-error">'+errorMessage+'</div>')
+            .insertAfter($element)
+            
         }
     }
 
@@ -107,6 +145,10 @@ $( document ).ready( () => {
     // but it seems to work correctly and the code doesnt look to wonky
     $designElement.on('change', function(e){
         const targetValue =  this.value
+        // show the color select element
+        $colorElement.show()
+        $colorLabel.show()
+
         // loop through and set selections for each option
         $colorOptions.each(function() {
             const currentValue = $(this).val()
@@ -131,10 +173,10 @@ $( document ).ready( () => {
     // I did this event handler before I read the pdf, 
     // but it seems to work correctly and the code doesnt look to wonky
     $activityElement.on('change', (e)=>{
-        // reset price
+        // reset price because we calculate the entire thing each pass
         totalPrice = 0;
         // run validation and apply error class if false
-        isActivitiesValid() ? toggleError($activityElement, 'remove') : toggleError($activityElement, 'add') 
+        isActivitiesValid() 
 
         //loop through all activities
         $activityCheckboxes.each((i)=>{
@@ -164,6 +206,8 @@ $( document ).ready( () => {
     $paymentElement.on('change', (e)=>{
         // turn them all off
         $('.credit-card, .bitcoin, .paypal').hide()
+        // toggle ccActive var for validation inclusion
+        e.target.value === 'credit-card' ? ccActive = true : ccActive = false
         // turn the right one on
         $('.'+e.target.value).show()
     })
@@ -178,32 +222,13 @@ $( document ).ready( () => {
             $otherElement.hide()
             $otherLabel.hide()
         }
-
     })
 
-    $nameElement.on('keyup', ()=>{
-         // run validation and apply error class if false
-        isNameValid() ? toggleError($nameElement, 'remove') : toggleError($nameElement, 'add')
-    })
-
-    $ccElement.on('keyup', ()=>{
-        // run validation and apply error class if false
-        isCreditCardValid() ? toggleError($ccElement, 'remove') : toggleError($ccElement, 'add')
-    })
-
-    $emailElement.on('keyup', ()=>{
-         // run validation and apply error class if false
-        isEmailValid() ? toggleError($emailElement, 'remove') : toggleError($emailElement, 'add')
-    })
-
-    $zipcodeElement.on('keyup', ()=>{
-        // run validation and apply error class if false
-        isZipCodeValid() ? toggleError($zipcodeElement, 'remove') : toggleError($zipcodeElement, 'add')
-    })
-
-   $cvvElement.on('keyup', ()=>{
-        // run validation and apply error class if false
-        isCVVValid() ? toggleError($cvvElement, 'remove') : toggleError($cvvElement, 'add')
-    })
+    $nameElement.on('keyup', ()=> isNameValid() )
+    $ccElement.on('keyup', ()=>isCreditCardValid())
+    $emailElement.on('keyup', ()=>isEmailValid())
+    $zipcodeElement.on('keyup', ()=>isZipCodeValid())
+    $cvvElement.on('keyup', ()=>isCVVValid())
+    $submitButton.on('click', (e) => validateForm(e))
 
 });
